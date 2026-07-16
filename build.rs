a1f90b5adb2311461e23211e71aabb645a767985
CaptainOachkatzl <dave.hofer@hotmail.com>
2026-07-16 12:55:20 +0200

    build.rs fixes

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::env;
 fn main() {
     const ASSETS: &str = "assets";
 
-    // Re-runs script if any files in res are changed
-    println!("cargo:rerun-if-changed={ASSETS}/*");
+    // Re-runs script if any asset files are changed
+    println!("cargo:rerun-if-changed={ASSETS}");
     copy_to_output(ASSETS, &env::var("PROFILE").unwrap()).expect("Could not copy");
 }
