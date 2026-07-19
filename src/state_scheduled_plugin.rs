82c49392eff535ec6ecc2ee1e36bdf1a81e43b7d
CaptainOachkatzl <dave.hofer@hotmail.com>
2026-07-19 14:29:29 +0200

    macro experiment

--- src/state_scheduled_plugin.rs
+++ src/state_scheduled_plugin.rs
@@ -3,6 +3,73 @@ use bevy::{
     prelude::*,
 };
 
+#[macro_export]
+macro_rules! state_scoped {
+    (
+        $app:expr,
+        $state:expr,
+        $($items:tt),* $(,)?
+    ) => {{
+        let app = $app;
+        $(
+            state_scoped!(@item app, $state, $items);
+        )*
+    }};
+
+    (@item $app:ident, $state:expr, OnEnter($system:expr)) => {
+        $app.add_systems(OnEnter($state), $system);
+    };
+
+    (@item $app:ident, $state:expr, OnExit($system:expr)) => {
+        $app.add_systems(OnExit($state), $system);
+    };
+
+    (@item $app:ident, $state:expr, RunInState($schedule:expr, $system:expr)) => {
+        $app.add_systems(
+            $schedule,
+            $system.run_if(in_state($state)),
+        );
+    };
+}
+
+struct TestPlugin;
+impl Plugin for TestPlugin {
+    fn build(&self, app: &mut App) {
+        state_scoped!(
+            app,
+            TestState::A,
+            OnEnter(test_system),
+            OnExit(test_system),
+            RunInState(Update, test_system),
+        );
+
+        let mut state_schedule = StateSchedule::new(app, TestState::A);
+        app.add_systems(OnEnter(TestState::A), test_system)
+            .add_systems(OnExit(TestState::A), test_system)
+            .add_systems(
+                Update,
+                test_system_with_params.run_if(in_state(TestState::A)),
+            );
+        state_schedule
+            .add_on_enter_systems(test_system)
+            .add_on_exit_systems(test_system)
+            .add_state_scoped_systems(Update, test_system);
+
+        app.add_systems(OnEnter(TestState::A), test_system)
+            .add_systems(OnExit(TestState::A), test_system)
+            .add_systems(
+                Update,
+                test_system_with_params.run_if(in_state(TestState::A)),
+            );
+
+        TestState::A.add_system_running_on_enter(app, test_system);
+
+        app.add_systems(OnEnter(TestState::A), test_system);
+
+        app.add_system_running_on_enter(TestState::A, test_system);
+    }
+}
+
 struct StateSchedule<'a, S: States> {
     state: S,
     app: &'a mut App,
@@ -96,28 +163,6 @@ impl<T: States> StatesExt for T {
 enum TestState {
     A,
     B,
-}
-
-struct TestPlugin;
-impl Plugin for TestPlugin {
-    fn build(&self, app: &mut App) {
-        let mut state_schedule = StateSchedule::new(app, TestState::A);
-
-        state_schedule
-            .add_on_enter_systems(test_system)
-            .add_on_exit_systems(test_system)
-            .add_state_scoped_systems(Update, test_system);
-
-        app.add_systems(OnEnter(TestState::A), test_system)
-            .add_systems(OnExit(TestState::A), test_system)
-            .add_systems(Update, test_system_with_params.run_if(in_state(TestState::A)));
-
-        TestState::A.add_system_running_on_enter(app, test_system);
-
-        app.add_systems(OnEnter(TestState::A), test_system);
-
-        app.add_system_running_on_enter(TestState::A, test_system);
-    }
 }
 
 fn test_system() {}
